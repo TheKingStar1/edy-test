29e4949bc6c5a68b39ea257dc3027aa59dbf210d
edy2004-fn <edy2004@outlook.com>
2020-02-04 23:48:31 +0200

    Create App2.js

---
+++ src/App2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Loc pentru stocat functii
+
+function App2() {
+
+  function sum(n) {
+    return (n*(n+1))/2
+  }
+}
+
+export default App2;
